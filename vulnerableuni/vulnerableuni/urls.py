aa578462eed43c8dba4c40c4f6401a7bb60f0582
abhhi-101 <birdawade11@gmai.com>
2022-11-25 10:35:35 -0500

    some changes in auth urls

--- vulnerableuni/vulnerableuni/urls.py
+++ vulnerableuni/vulnerableuni/urls.py
@@ -14,11 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from students import views as student_views
+from django.conf import settings
+from django.conf.urls.static import static
+from students import views as students_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', students_views.register, name='register'),
+    path('profile/', students_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='students/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='students/logout.html'), name='logout'),
     path('', include('administrator.urls')),
-    path('register/', student_views.register, name='register'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
