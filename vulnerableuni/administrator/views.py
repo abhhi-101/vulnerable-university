ab7303bbad2ca27d6db2dd8b0b88bc1006152088
abhhi-101 <birdawade11@gmai.com>
2022-11-25 07:38:40 -0500

    removed css error

--- vulnerableuni/administrator/views.py
+++ vulnerableuni/administrator/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
 # Create your views here.
+
+def home(request):
+    context = {
+        'title':'University Home Page'
+    }
+    return render(request, 'administrator/home.html', context)
+
+def about(request):
+    context = {
+        'title': 'Blogging using template'
+    }
+    return render(request, 'blog/about.html', context)
+
+def xss(request):
+    if request.user.is_authenticated:
+        q=request.GET.get('q','')
+        #f=FAANG.objects.filter(company=q)
+        if f:
+            args={"company":f[0].company,"ceo":f[0].info_set.all()[0].ceo,"about":f[0].info_set.all()[0].about}
+            return render(request,'administrator/xss.html',args)
+        else:
+            return render(request,'administrator/xss.html', {'query': q})
+    else:
+        return redirect('login')
