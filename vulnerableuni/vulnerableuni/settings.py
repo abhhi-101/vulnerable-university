7d99bbf0a6b9c14b183f1a86a589cd13aafcc59a
abhhi-101 <birdawade11@gmai.com>
2022-11-25 10:31:52 -0500

    added authentication

--- vulnerableuni/vulnerableuni/settings.py
+++ vulnerableuni/vulnerableuni/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'administrator',
+    'students',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- vulnerableuni/vulnerableuni/urls.py
+++ vulnerableuni/vulnerableuni/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from students import views as student_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('administrator.urls')),
+    path('register/', student_views.register, name='register'),
 ]
